ff471a06f10a1b3b302db038b4a43e27ae917cf7
dmelles <dmelles@crystal.csus.org>
2016-02-05 21:47:54 -0800

    Added drawing for Square

--- Square.py
+++ Square.py
@@ -1,3 +1,10 @@
+from graphics import *
 class Square:
-    def __init__(self,color):
+    def __init__(self,color,x,y):
         self.piece = 0
+        l = 50
+        self.graphicsSquare = Rectangle(Point(x,y),Point(x+l,y+l))
+        
+
+    def draw(self,window):
+        self.graphicsSquare.draw(window)
