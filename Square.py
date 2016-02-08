4dd5b9ea330436e15ef42058c12a8854600200ba
dmelles <dmelles@crystal.csus.org>
2016-02-08 09:03:27 -0800

    Added highlight color

--- Square.py
+++ Square.py
@@ -1,10 +1,19 @@
 from graphics import *
 class Square:
-    def __init__(self,color,x,y):
+    def __init__(self,color,highlightColor,x,y):
         self.piece = 0
         l = 50
         self.graphicsSquare = Rectangle(Point(x,y),Point(x+l,y+l))
+        self.graphicsSquare.setOutline(color)
+        self.graphicsSquare.setFill(color)
+        self.highlightColor = highlightColor
+        self.color = color
         
 
     def draw(self,window):
         self.graphicsSquare.draw(window)
+
+    def highlight(self):
+        self.graphicsSquare.setFill(self.highlightColor)
+    def unHighlight(self):
+        self.graphicsSquare.setFill(self.color)
